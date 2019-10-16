a71346f3ad727b7656ab69a5ac2f86efbf180c84
Jonas Van Crombrugge <jonas.van.crombrugge@int.micrelec.be>
2019-10-16 09:37:48 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow
+
+
+    Dim var As Double
+
+    Public Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Console.WriteLine("hello world!")
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        Console.WriteLine("hello world!")
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+        Console.WriteLine("hello world!")
+        txtButtonHW.Text = "Aaaaa"
+    End Sub
+End Class
